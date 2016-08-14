936aaf391dba3b2a8b914238fbac7ce0f79edb58
Jared Friese <jared.friese@gmail.com>
2016-08-14 08:24:31 -0700

    Print verbose in test action

--- script/uitests.go
+++ script/uitests.go
@@ -14,7 +14,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	uiTestCommand := exec.Command("xcodebuild", "test", "-workspace", "FleetUI.xcworkspace", "-scheme", "FleetUI", "-destination", "platform=iOS Simulator,OS=9.3,name=iPhone 6")
+	uiTestCommand := exec.Command("xcodebuild", "-verbose", "test", "-workspace", "FleetUI.xcworkspace", "-scheme", "FleetUI", "-destination", "platform=iOS Simulator,OS=9.3,name=iPhone 6")
 	uiTestCommand.Stdout = stdOut
 	uiTestCommand.Stderr = stdErr
 	uiTestErr := uiTestCommand.Run()
