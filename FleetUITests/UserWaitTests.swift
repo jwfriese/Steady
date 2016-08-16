3ad53a163c26df7268e6f9017ec292f6862d56b1
Jared Friese <jared.friese@gmail.com>
2016-08-16 05:36:25 -0700

    Remove unnecessary sleep and animation disable in a test

--- FleetUITests/UserWaitTests.swift
+++ FleetUITests/UserWaitTests.swift
@@ -9,14 +9,12 @@ class UserWaitTests: XCTestCase {
         super.setUp()
 
         continueAfterFailure = false
-        UIView.setAnimationsEnabled(false)
         user = FleetUI.createUser(testCase: self)
         user.startTesting()
     }
 
     func testActionsWillWaitBeforeGivingUpAndFailing() {
         user.tapButtonWithText("Puppy")
-        sleep(1)
         user.tapButtonWithText("Show Puppies")
         user.tapButtonWithText("Corgi") // This is expected to appear after a period of time
         user.expectsTo(findText("It's a Corgi!"))
